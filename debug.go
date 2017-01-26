ddcfe8387445be80735cd98aa9642e8eae4e6733
Shuhei Kubota <kubota.shuhei@gmail.com>
2017-01-26 14:15:38 +0900

    add debug log destination by env POMI_LOG
    POMI_LOG=path_to_file
      append logs to path_to_file
    POMI_LOG=*path_to_file
      overwrite logs to path_to_file

--- debug.go
+++ debug.go
@@ -4,9 +4,23 @@ package main
 
 import (
 	"bitbucket.org/shu/log"
+	"os"
 )
 
 func init() {
 	log.SetLevel(log.DebugLevel)
 	log.SetFlags(log.DefaultHeader | log.Shortfile)
+
+	if dest := os.Getenv("POMI_LOG"); dest != "" {
+		flags := os.O_CREATE | os.O_APPEND
+		if dest[0] == '*' {
+			flags = os.O_CREATE | os.O_RDWR
+			dest = dest[1:]
+		}
+		f, err := os.OpenFile(dest, flags, 0x600)
+		if err != nil {
+			panic(err)
+		}
+		log.SetOutput(f)
+	}
 }
